2e42fcd675a4b7695c3d67506de7d77e75756632
Ariel123-beep <ariel1234alas@outlook.com>
2022-02-21 14:51:59 -0600

    Update

---
+++ Practica_Codigo/Practica_Codigo/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Ejemplo
+{
+   
+
+    public static void Main()
+    {
+        Stack<string> personas = new Stack<string>();
+        personas.Push("Jordy Enrique Rivas Castro");
+        personas.Push("Wendy Lisseth Chacon Pineda");
+        personas.Push("Manuel Humberto Lopez Castro");
+        personas.Push("Ariel Edilberto Alas Rubio");
+        personas.Push("Cristian Alexander Lopez Menjivar");
+        personas.Push("Yacquelyn Alexandra Rivera Alfaro");
+
+        foreach (string person in personas)
+        {
+            Console.WriteLine(person);
+        }
+
+        Console.WriteLine("\nPeek/El primer nombre a desapilar será : {0}",
+            personas.Peek());
+        Console.WriteLine("Popping/ Eliminando '{0}'", personas.Pop());
+        Console.WriteLine("\nPeek/viendo en el siguiente item a desapilar: {0}",
+            personas.Peek());
+        Console.WriteLine("Popping/ Eliminando '{0}'", personas.Pop());
+
+        Stack<string> Personas2 = new Stack<string>(personas.ToArray());
+
+        Console.WriteLine("\nContenido de la primer copia:");
+        foreach (string person in Personas2)
+        {
+            Console.WriteLine(person);
+        }
+
+
+        string[] array2 = new string[personas.Count * 2];
+        personas.CopyTo(array2, personas.Count);
+
+        Stack<string> Personas3 = new Stack<string>(array2);
+
+        Console.WriteLine("\nContenido de la segunda copia, con duplicados y nulos:");
+        foreach (string person in Personas3)
+        {
+            Console.WriteLine(person);
+        }
+
+        Console.WriteLine("\nVerificando si la pila # 2 contiene a la persona Yacquelyn Alexandra Rivera Alfaro ");
+        Console.WriteLine("\nLa pila #2 contiene \"Yacquelyn Alexandra Rivera Alfaro\" = {0}",
+        Personas2.Contains("Yacquelyn Alexandra Rivera Alfaro"));
+
+        Console.WriteLine("\nLimpiando la pila #2");
+
+        Console.WriteLine("\nPersonas2.Clear()");
+        Personas2.Clear();
+        Console.WriteLine("\nPersonas2.Count = {0}", Personas2.Count);
+
+
+
+    }
+}
+
---
+++ Pratica_Cola/Pratica_Cola/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+public class EjemploQueue
+{
+
+    public static void Main()
+    {
+
+        Queue miQ = new Queue();
+        miQ.Enqueue("Cuatro");
+        miQ.Enqueue("Ocho");
+        miQ.Enqueue("Doce");
+        miQ.Enqueue("Dieciseis");
+
+        Console.WriteLine("\nElementos guardados en la cola de nombre miQ");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("\nConteo de la cantidad de elementos en la cola {0}");
+        Console.WriteLine(miQ.Count);
+        Console.Write("\nValores dentro de la cola:");
+        PrintValues(miQ);
+
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Removiendo un elemento (Dequeue){0}");
+        Console.WriteLine("{0}", miQ.Dequeue());
+
+        Console.Write("\nMostrando valores de Queue:");
+        PrintValues(miQ);
+
+
+        Console.WriteLine("");
+
+
+        Console.WriteLine("Removiendo otro  (Dequeue)");
+        Console.WriteLine(" {0}", miQ.Dequeue());
+
+
+        Console.WriteLine("");
+
+        Console.Write("\nMostrando valores de la Queue:");
+        PrintValues(miQ);
+
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Mostrando primer elemento sin removerlos (Peek)");
+
+        Console.WriteLine("{0}", miQ.Peek());
+
+        Console.WriteLine("");
+
+        Console.Write("\nMostrando los valores de la Queue:");
+        PrintValues(miQ);
+
+        Console.WriteLine("");
+
+
+        Console.WriteLine("\nCount/ Conteo de los valores: {0}");
+        Console.WriteLine(miQ.Count);
+
+
+        Console.WriteLine("");
+
+        Console.Write("\nValores en la cola:");
+        PrintValues(miQ);
+    }
+
+    public static void PrintValues(IEnumerable myCollection)
+    {
+        foreach (Object obj in myCollection)
+            Console.Write("\t {0}", obj);
+        Console.WriteLine();
+    }
+
+
+}
